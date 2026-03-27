a6eb8a132a72dc6d28518f4a6daf21ce7420c2ac
Glaucia Grossi <glau.kobashigawa.jp@gmail.com>
2026-03-27 20:07:50 +0900

    sounds logic

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const html = document.querySelector('html');
-
+// buttons
 const focusBtn = document.querySelector('.app__card-button--foco');
 const shortBtn = document.querySelector('.app__card-button--curto');
 const longBtn = document.querySelector('.app__card-button--longo');
 const buttons = document.querySelectorAll('.app__card-button');
 
+// sounds
 const playMusic = document.querySelector('#alternar-musica');
 const music = new Audio('/sounds/luna-rise-part-one.mp3');
 music.loop = true
+
+const playSound = new Audio('/sounds/play.wav');
+const pauseSound = new Audio('/sounds/pause.mp3');
+const endSound = new Audio('/sounds/beep.mp3');
+
+let timePassedSeconds = 5
+const startPauseBt = document.querySelector('#start-pause')
+let intervalId = null
 
 playMusic.addEventListener('change', ()=> {
   if (music.paused) {
@@ -17,6 +26,7 @@ playMusic.addEventListener('change', ()=> {
   }
 })
 
+// change theme
 focusBtn.addEventListener('click', () => {
   changeContext('foco')
   focusBtn.classList.add('active')
@@ -37,4 +47,33 @@ function changeContext(context) {
         context.classList.remove('active')
     })
     html.setAttribute('data-contexto', context)
+}
+
+// countdown
+const countdown = () => {
+  if(timePassedSeconds <= 0){
+    reset()
+    endSound.play()
+    alert("Time's up.")
+    return
+  }
+    timePassedSeconds -= 1
+    console.log('Temporizador: ' + timePassedSeconds)
+}
+
+startPauseBt.addEventListener('click', startCount)
+
+function startCount() {
+  if(intervalId){
+    reset()
+    pauseSound.play()
+    return
+  }
+  playSound.play()
+    intervalId = setInterval(countdown, 1000)
+}
+
+function reset() {
+    clearInterval(intervalId)
+    intervalId = null
 }

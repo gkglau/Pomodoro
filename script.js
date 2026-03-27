0f95b693feb5627e69b187226eda2ff663f51764
Glaucia Grossi <glau.kobashigawa.jp@gmail.com>
2026-03-27 19:27:31 +0900

    audio and button status

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-const html = document.querySelector('html')
-const focusBtn = document.querySelector('.app__card-button--foco')
-const shortBtn = document.querySelector('.app__card-button--curto')
-const longBtn = document.querySelector('.app__card-button--longo')
+const html = document.querySelector('html');
+
+const focusBtn = document.querySelector('.app__card-button--foco');
+const shortBtn = document.querySelector('.app__card-button--curto');
+const longBtn = document.querySelector('.app__card-button--longo');
+const buttons = document.querySelectorAll('.app__card-button');
+
+const playMusic = document.querySelector('#alternar-musica');
+const music = new Audio('/sounds/luna-rise-part-one.mp3');
+music.loop = true
+
+playMusic.addEventListener('change', ()=> {
+  if (music.paused) {
+    music.play()
+  } else {
+    music.pause()
+  }
+})
 
 focusBtn.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'foco')
+  changeContext('foco')
+  focusBtn.classList.add('active')
 })
 
 shortBtn.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-curto')
+  changeContext('descanso-curto')
+  shortBtn.classList.add('active')
 })
 
 longBtn.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-longo')
+  changeContext('descanso-longo')
+  longBtn.classList.add('active')
 })
+
+function changeContext(context) {
+  buttons.forEach(function (context) {
+        context.classList.remove('active')
+    })
+    html.setAttribute('data-contexto', context)
+}

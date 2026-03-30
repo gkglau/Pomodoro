b335b6a55bee4cbe3dc52766c7fdedd9ca1a311c
Glaucia Grossi <glau.kobashigawa.jp@gmail.com>
2026-03-30 21:31:02 +0900

    crud start

--- script-crud.js
+++ script-crud.js
@@ -1,1 +1,18 @@
-document.querySelector()
+const btnAddTask = document.querySelector('.app__button--add-task')
+const formAddTask = document.querySelector('.app__form-add-task')
+const textarea = document.querySelector('.app__form-add-task')
+
+const tasks = []
+
+btnAddTask.addEventListener('click', () => {
+  formAddTask.classList.toggle('hidden')
+} )
+
+formAddTask.addEventListener('submit',(event) => {
+  event.preventDefault();
+  const task = {
+    descript: textarea.value
+  }
+  tasks.push(task)
+  localStorage.setItem('tasks', tasks)
+})
